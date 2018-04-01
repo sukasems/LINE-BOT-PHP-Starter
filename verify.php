a24fc2cae57ca11c95c7ca15c151335bba6cc3c2
sukasem.siw <sukasem.siw@gmail.com>
2018-04-01 23:40:25 +0700

    update v1 to v3

--- verify.php
+++ verify.php
@@ -1,6 +1,6 @@
 <?php
 $access_token = 'X8zC4jd3XJ4nESJ1cvmzvL17zFpgMKMt21/eRda35rWUZwfRuSTHVas/8PSYzwevEossSWUQa628N9dRZTyL6uwcl1knxpgfNSl2jzXV7WtTp+B462/AggG5OWPy5MeGPjZ/VF2t0Jei6dIKk/ai2gdB04t89/1O/w1cDnyilFU=';
-$url = 'https://api.line.me/v1/oauth/verify';
+$url = 'https://api.line.me/v3/oauth/verify';
 $headers = array('Authorization: Bearer ' . $access_token);
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
