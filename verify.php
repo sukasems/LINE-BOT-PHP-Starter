e93b7d4bdd910f647232fed913c2bf83e55d1eba
sukasem.siw <sukasem.siw@gmail.com>
2018-04-01 23:54:52 +0700

    update url

--- verify.php
+++ verify.php
@@ -1,7 +1,7 @@
 <?php
 echo 'v3';
 $access_token = 'X8zC4jd3XJ4nESJ1cvmzvL17zFpgMKMt21/eRda35rWUZwfRuSTHVas/8PSYzwevEossSWUQa628N9dRZTyL6uwcl1knxpgfNSl2jzXV7WtTp+B462/AggG5OWPy5MeGPjZ/VF2t0Jei6dIKk/ai2gdB04t89/1O/w1cDnyilFU=';
-$url = 'https://api.line.me/v3/oauth/verify';
+$url = 'https://access.line.me/oauth2/v2.1/verify';
 $headers = array('Authorization: Bearer ' . $access_token);
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -10,3 +10,4 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
 echo $result;
+
