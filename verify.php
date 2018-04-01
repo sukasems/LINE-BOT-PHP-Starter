75e0d74f04f6a1afd342eeb98365134f88e44f6b
sukasem.siw <sukasem.siw@gmail.com>
2018-04-01 23:34:01 +0700

    add verify.php

---
+++ verify.php
@@ -0,0 +1,11 @@
+<?php
+$access_token = 'X8zC4jd3XJ4nESJ1cvmzvL17zFpgMKMt21/eRda35rWUZwfRuSTHVas/8PSYzwevEossSWUQa628N9dRZTyL6uwcl1knxpgfNSl2jzXV7WtTp+B462/AggG5OWPy5MeGPjZ/VF2t0Jei6dIKk/ai2gdB04t89/1O/w1cDnyilFU=';
+$url = 'https://api.line.me/v1/oauth/verify';
+$headers = array('Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result;
